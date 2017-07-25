702ae601a89cce12a4b39462f6fcbd9d1d93c841
Ankit kumar <desidevel@gmail.com>
2017-07-25 04:26:40 +0000

    models.py edited online with Bitbucket (Model added userAddDiet on line 76

--- viewscontroller/models.py
+++ viewscontroller/models.py
@@ -72,3 +72,6 @@ class music(models.Model):
     url = models.CharField(max_length=2000)
     def __str__(self):
 		return self.title
+
+class userAddDiet(models.Model):
+	dietName = models.CharField(max_length=200)
